adfd2aede4b3567ad11f377490507755a22f54fc
Eliel <eliel.garcia@gmail.com>
2020-02-11 22:41:20 -0300

    projeto adicionado

---
+++ HelloCodenation/src/br/com/Principal.java
@@ -0,0 +1,20 @@
+package br.com;
+
+import br.com.tarefas.Tarefa;
+
+
+public class Principal {
+    public static void main(String[] args){
+        System.out.println("minhas tarefas");
+        System.out.println("-------------------");
+        Tarefa tarefa1 = new Tarefa("minha tarefa");
+        tarefa1.exibirTarefa();
+
+
+
+    }
+
+
+
+
+}
