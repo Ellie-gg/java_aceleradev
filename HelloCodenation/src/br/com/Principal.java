e21af0213b96c112ad74e71796009e33928b8f39
Eliel <eliel.garcia@gmail.com>
2020-02-13 23:13:37 -0300

    Array List adicionado

--- HelloCodenation/src/br/com/Principal.java
+++ HelloCodenation/src/br/com/Principal.java
@@ -1,20 +1,25 @@
 package br.com;
 
 import br.com.tarefas.Tarefa;
+import br.com.tarefas.ListaTarefas;
+
+
 
 
 public class Principal {
     public static void main(String[] args){
         System.out.println("minhas tarefas");
         System.out.println("-------------------");
-        Tarefa tarefa1 = new Tarefa("minha tarefa");
-        tarefa1.exibirTarefa();
-
+        ListaTarefas tarefas = new ListaTarefas();
+        Tarefa tarefa1 = new Tarefa("Regar as plantas");
+        Tarefa tarefa2 = new Tarefa("Estudar");
+        Tarefa tarefa3 = new Tarefa("Trabalhar");
+        tarefas.adicionar(tarefa1);
+        tarefas.adicionar(tarefa2);
+        tarefas.adicionar(tarefa3);
+        tarefas.exibirTarefas();
 
 
     }
-
-
-
 
 }
---
+++ HelloCodenation/src/br/com/tarefas/ListaTarefas.java
@@ -0,0 +1,23 @@
+package br.com.tarefas;
+
+import java.util.ArrayList;
+
+public class ListaTarefas {
+
+    ArrayList<Tarefa> tarefas = new ArrayList<>();
+
+    public void adicionar(Tarefa tarefa){
+        this.tarefas.add(tarefa);
+    }
+
+    public void remover(int posicao){
+        this.tarefas.remove(posicao);
+
+    }
+
+    public void exibirTarefas(){
+        for (Tarefa tarefa: tarefas){
+            tarefa.exibirTarefa();
+        }
+    }
+}
